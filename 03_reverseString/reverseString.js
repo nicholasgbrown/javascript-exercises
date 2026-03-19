a8e755f8dea9f0e60c1af5764cc0d3097881a211
Nicholas Brown <nicholasgbrown@users.noreply.github.com>
2026-03-19 19:28:02 +0000

    update

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (myString) {
+let result = myString.split("").reverse().join("");
+return result;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(a, ...args) {
+
+let result = a.filter((element) => { 
+    return !args.includes(element);
+});
+
+return result;
+
 };
+
+// console.log(removeFromArray(["red", "yellow"], "red"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
